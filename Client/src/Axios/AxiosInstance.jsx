e6879b52802aba95cf7b21b7257fdce9a014859c
prit vyas <vyasprit962@gmail.com>
2025-09-22 18:56:36 +0530

    Final Production

--- Client/src/Axios/AxiosInstance.jsx
+++ Client/src/Axios/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from "axios"
 const AxiosInstance = axios.create({
-    baseURL:import.meta.env.BACKEND_URI,
+    baseURL:"https://cloth-printing.onrender.com",
     withCredentials: true,
      headers: {
     "Content-Type": "application/json",
