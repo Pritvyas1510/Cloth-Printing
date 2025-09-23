e392100b2763b60e57101a5cc1a93abeb09b25fb
prit vyas <vyasprit962@gmail.com>
2025-09-23 13:06:23 +0530

    Final Production

--- Client/src/Constent/Data.js
+++ Client/src/Constent/Data.js
@@ -6,7 +6,6 @@ export const roleLinks = {
   user: [
     { path: "/", label: "Home" },
     { path: "/design", label: "Design" },
-    { path: "/contact", label: "Contact" },
     { path: "/about", label: "About" },
   ],
   admin: [
